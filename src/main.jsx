46411b0405bcaa69340078586cb61d85c9c30ab7
JeampierSmith <jeampiersqc@gmail.com>
2025-05-29 22:51:08 -0400

    fix: arreglo de responsiidad de la cartas de la tiendas aliadas y formulario

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import './i18n/i18n.js'
@@ -19,5 +19,5 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter basename="/Bolivia-ludica">
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 )
